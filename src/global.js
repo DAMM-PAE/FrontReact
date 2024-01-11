bc52c7c52c6598fd759ba864e1568bc35698bba8
HaopengALS <haopeng@alstp.com>
2024-01-11 15:17:43 +0100

    deploy mod

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-//export const baseUrl = 'http://nattech.fib.upc.edu:40540'
-export const baseUrl = 'http://127.0.0.1:8000'
+export const baseUrl = 'http://nattech.fib.upc.edu:40540'
+//export const baseUrl = 'http://127.0.0.1:8000'
